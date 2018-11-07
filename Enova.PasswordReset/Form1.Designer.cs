0b38dc687d20660e5d2af554850cd248ab266dd2
Jakub Kamiński <mystticdruid@live.com>
2018-11-07 10:44:31 +0100

    .

---
+++ Enova.PasswordReset/Form1.Designer.cs
@@ -0,0 +1,161 @@
+﻿namespace Enova.PasswordReset
+{
+	partial class FrmPasswordReset
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FrmPasswordReset));
+            this.tPass = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btnLicz = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.WAlabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // tPass
+            // 
+            this.tPass.Location = new System.Drawing.Point(12, 68);
+            this.tPass.Name = "tPass";
+            this.tPass.Size = new System.Drawing.Size(152, 20);
+            this.tPass.TabIndex = 2;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(170, 75);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(68, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Nowe hasło.";
+            // 
+            // btnLicz
+            // 
+            this.btnLicz.Location = new System.Drawing.Point(12, 94);
+            this.btnLicz.Name = "btnLicz";
+            this.btnLicz.Size = new System.Drawing.Size(152, 23);
+            this.btnLicz.TabIndex = 7;
+            this.btnLicz.Text = "Wykonaj";
+            this.btnLicz.UseVisualStyleBackColor = true;
+            this.btnLicz.Click += new System.EventHandler(this.btnLicz_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 120);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(251, 26);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "\r\nPo tej operacji wyłącz i włącz ponownie enova365.";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(12, 12);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(152, 21);
+            this.comboBox1.TabIndex = 9;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Location = new System.Drawing.Point(12, 41);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(152, 21);
+            this.comboBox2.TabIndex = 10;
+            this.comboBox2.SelectedIndexChanged += new System.EventHandler(this.comboBox2_SelectedIndexChanged);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(171, 20);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(31, 13);
+            this.label5.TabIndex = 12;
+            this.label5.Text = "Baza";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(170, 49);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(48, 13);
+            this.label6.TabIndex = 13;
+            this.label6.Text = "Operator";
+            // 
+            // WAlabel
+            // 
+            this.WAlabel.AutoSize = true;
+            this.WAlabel.BackColor = System.Drawing.Color.Red;
+            this.WAlabel.Location = new System.Drawing.Point(242, 12);
+            this.WAlabel.Name = "WAlabel";
+            this.WAlabel.Size = new System.Drawing.Size(10, 13);
+            this.WAlabel.TabIndex = 14;
+            this.WAlabel.Text = " ";
+            // 
+            // FrmPasswordReset
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(266, 152);
+            this.Controls.Add(this.WAlabel);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.btnLicz);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.tPass);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "FrmPasswordReset";
+            this.Text = "Reseter haseł enova365";
+            this.Load += new System.EventHandler(this.FrmPasswordReset_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+		}
+
+		#endregion
+		private System.Windows.Forms.TextBox tPass;
+		private System.Windows.Forms.Label label3;
+		private System.Windows.Forms.Button btnLicz;
+		private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label WAlabel;
+    }
+}
+
---
+++ Enova.PasswordReset/Form1.cs
@@ -0,0 +1,198 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Enova.PasswordReset
+{
+	public partial class FrmPasswordReset : Form
+	{
+		public FrmPasswordReset()
+		{
+			InitializeComponent();
+		}
+
+		static byte[] passwordKey = new byte[] { 54, 94, 185, 184, 123, 100, 223, 213, 253, 51, 5, 87, 27, 64, 88, 111, 67, 94, 63, 82, 175, 129, 134, 211 };
+		static MACTripleDES passwordDES = new MACTripleDES(passwordKey);
+        public static string dbGuid;        
+        public static string opGuid;
+        public static string newPass;
+
+		private void btnLicz_Click(object sender, EventArgs e)
+		{
+			try
+			{
+				byte[] arrayDbGuid = new Guid(dbGuid).ToByteArray();
+				byte[] arrayPass = Encoding.Unicode.GetBytes(tPass.Text);
+				byte[] arrayOperatorGuid = new Guid(opGuid).ToByteArray();
+				byte[] arrayOutput = new byte[arrayDbGuid.Length + arrayPass.Length + arrayOperatorGuid.Length];
+				arrayDbGuid.CopyTo(arrayOutput, 0);
+				arrayPass.CopyTo(arrayOutput, arrayDbGuid.Length);
+				arrayOperatorGuid.CopyTo(arrayOutput, arrayDbGuid.Length + arrayPass.Length);
+
+				newPass = ByteArrayToString(passwordDES.ComputeHash(arrayOutput));
+                Commit();
+			}
+			catch(Exception ex)
+			{
+				MessageBox.Show("Błąd podczas generowania nowego hasła. Sprawdź czy poprawnie wkleiłeś Guidy.");
+			}
+		}
+
+		static string ByteArrayToString(byte[] t)
+		{
+			StringBuilder stringBuilder = new StringBuilder();
+			for (int i = 0; i < t.Length; i++)
+			{
+				ByteToHex(t[i], stringBuilder);
+			}
+			return stringBuilder.ToString();
+		}
+
+		static void ByteToHex(byte b, StringBuilder sb)
+		{
+			sb.Append(getCharUpp((b & 240) >> 4));
+			sb.Append(getCharUpp((int)(b & 15)));
+		}
+
+		static char getCharUpp(int v)
+		{
+			if (v < 10)
+			{
+				return (char)(48 + v);
+			}
+			return (char)(65 + v - 10);
+		}        
+        private void FrmPasswordReset_Load(object sender, EventArgs e)
+        {
+            ComboBox cbx = comboBox1;
+            cbx.DataSource = GetDataBaseList();
+        }       
+        
+        public List<string> GetDataBaseList()
+        {            
+            List<string> list = new List<string>();
+            try
+            {
+                using (SqlConnection conn = getDBConnection("master"))
+                {
+                    conn.Open();
+                    using (SqlCommand cmd = new SqlCommand("SELECT name from sys.databases", conn))
+                    {
+                        using (IDataReader dr = cmd.ExecuteReader())
+                        {
+                            while (dr.Read())
+                                list.Add(dr[0].ToString());
+                        }
+                    }
+                }
+                list.Sort(delegate (string x, string y)
+                {
+                    return x.CompareTo(y);
+                });
+            }
+            catch { MessageBox.Show("Nie udało się zalogować do serwera"); }
+            return list;
+        }
+        public SqlConnection getDBConnection(string DataBaseName)
+        {
+            string datasource = Properties.Settings1.Default.DataSource;
+            if (Properties.Settings1.Default.WindowsAuth)
+            {
+                WAlabel.BackColor = Color.Green;
+                try
+                {
+                    SqlConnection conn = new SqlConnection(@"Data Source=" + datasource + ";Initial Catalog=" + DataBaseName + ";Integrated Security=True;");
+                    return conn;
+                }
+                catch { return null; }
+            }
+            else
+            {
+                try
+                {
+                    
+                    bool security = Properties.Settings1.Default.PersistSecurityInfo;
+                    string user = Properties.Settings1.Default.User;
+                    string password = Properties.Settings1.Default.Password;
+                    SqlConnection conn = new SqlConnection(@"Data Source=" + datasource + ";Initial Catalog=" + DataBaseName + ";Persist Security Info=" + security + ";User ID=" + user + ";Password=" + password + "");
+                    return conn;
+                }
+                catch { return null; }
+            }
+            
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            try
+            {
+                using (SqlConnection connection = getDBConnection(comboBox1.SelectedValue.ToString()))
+                {
+                    connection.Open();                    
+                    using (SqlCommand cmd = new SqlCommand("SELECT Value FROM dbo.SystemInfos WHERE Ident = 7", connection))
+                    {
+                        using (IDataReader dr = cmd.ExecuteReader())
+                        {
+                            while (dr.Read())
+                                dbGuid = dr[0].ToString();
+                        }
+                    }
+                    List<string> list = new List<string>();
+                    using (SqlCommand cmd = new SqlCommand("SELECT Name FROM dbo.Operators", connection))
+                    {
+                        using (IDataReader dr = cmd.ExecuteReader())
+                        {
+                            while (dr.Read())
+                                list.Add(dr[0].ToString());
+                        }
+                    }
+                    comboBox2.DataSource = list;
+                    
+                }    
+                            
+            }
+            catch(Exception ex) { }
+        }
+        private void comboBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            try
+            {
+                using (SqlConnection connection = getDBConnection(comboBox1.SelectedValue.ToString()))
+                {
+                    connection.Open();    
+                    using (SqlCommand cmd = new SqlCommand("SELECT Guid FROM dbo.Operators WHERE Name like '"+comboBox2.SelectedValue.ToString()+"' ", connection))
+                    {
+                        using (IDataReader dr = cmd.ExecuteReader())
+                        {
+                            while (dr.Read())
+                                opGuid = dr[0].ToString();
+                        }
+                    }                    
+                }
+
+            }
+            catch (Exception ex) { }
+        }                
+        public void Commit()
+        {
+            try
+            {
+                using (SqlConnection connection = getDBConnection(comboBox1.SelectedValue.ToString()))
+                {
+                    connection.Open();
+                    SqlCommand cmd = new SqlCommand("UPDATE Operators SET Password = '" + newPass + "' WHERE Name like '" + comboBox2.SelectedValue.ToString() + "' ", connection);
+                    cmd.ExecuteNonQuery();
+                }
+                MessageBox.Show("Wykonano!");
+            }
+            catch (Exception ex) { MessageBox.Show("Błąd!"+ex); }
+        }
+    }
+}
