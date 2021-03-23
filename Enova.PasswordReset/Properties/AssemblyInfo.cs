5cfde1334322b8373bffd9a662b3ab4968bad1f5
Mysttic <19372942+Mysttic@users.noreply.github.com>
2021-03-23 10:18:22 +0100

    .Automatyzacja wydawania wersji

--- Enova.PasswordReset/Properties/AssemblyInfo.cs
+++ Enova.PasswordReset/Properties/AssemblyInfo.cs
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("2103.1.23.0916")]
+[assembly: AssemblyFileVersion("2103.1.23.0916")]
